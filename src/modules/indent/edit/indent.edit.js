5da66660d3f57c0bef78105cf5a60f33f7a90517
zhuzheng <23194749@qq.com>
2016-12-25 02:45:50 +0800

    提交代码

--- src/modules/indent/edit/indent.edit.js
+++ src/modules/indent/edit/indent.edit.js
@@ -1,25 +1,35 @@
 require('./indent.edit.less');
 
-var utils   = require('../../core/utils');
+var utils = require('../../core/utils');
 var api = require('../../core/api');
-
+var indentEditBaseModule = require('./indent.edit.base.js');
+var indentEditDetailModule = require('./indent.edit.detail.js');
+var indentEditBaseTemplate = require('./indent.edit.base.tpl.html');
+var indentEditDetailTemplate = require('./indent.edit.detail.tpl.html');
 
 var indentEditModule = {
     pageInit: function (page) {
 		this.getIndent(page.context.id);
 
-		this.bindEvents();
-
-		this.initRenderNewBaseItem();
-		this.initRenderNewDetailItem();
-
-		indentNewBaseModule.init();
-		indentNewDetailModule.init();
+		// this.bindEvents();
     },
 
     getIndent: function (id) {
+    	var self = this;
+
         api.getIndent(function (data) {
         	log(data)
+
+			var src = {
+				tradeCompanyId: data.trade_,
+				supplierId: parseInt(data.supplier_)
+			};
+
+			self.initRenderEditBaseItem(src);
+			self.initRenderEditDetailItem();
+
+			// indentNewBaseModule.init();
+			// indentNewDetailModule.init();
             // var code = data.err_code;
             // if (code === undefined || code === null || isNaN(code)) {
             //     alert('code is null');
@@ -38,7 +48,19 @@ var indentEditModule = {
             // var output = appFunc.renderTpl(template, renderData);
             // $$('#commentContent').html(output);
         }, gUser.employee_id, id);
-    }
+    },
+
+	initRenderEditBaseItem: function(baseSource) {
+    	log(gRepository)
+    	log(baseSource)
+		var output = utils.renderTpl(indentEditBaseTemplate, {repository: gRepository, source: baseSource});
+		$$('#tabBase').html(output);
+	},
+
+	initRenderEditDetailItem: function() {
+		var output = utils.renderTpl(indentEditDetailTemplate, {});
+		$$('#tabDetail').html(output);
+	},
 };
 
 module.exports = indentEditModule;
--- src/modules/main.js
+++ src/modules/main.js
@@ -8,48 +8,65 @@ var home = require('./home/home');
 var login = require('./login/login');
 
 var app = {
-    initialize: function() {
-		document.ontouchmove = function(e){ e.preventDefault(); };
+	initialize: function () {
+		document.ontouchmove = function (e) {
+			e.preventDefault();
+		};
 		this.bindEvents();
-    },
+	},
 
-    bindEvents: function() {
-        if (utils.isPhonegap()) {
-            document.addEventListener('deviceready', this.onDeviceReady, false);
-        } else {
-            window.onload = this.onDeviceReady();
-        }
-    },
+	bindEvents: function () {
+		if (utils.isPhonegap()) {
+			document.addEventListener('deviceready', this.onDeviceReady, false);
+		} else {
+			window.onload = this.onDeviceReady();
+		}
+	},
 
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
+	onDeviceReady: function () {
+		app.receivedEvent('deviceready');
+	},
 
-    receivedEvent: function(event) {
-        switch (event) {
-            case 'deviceready':
-                app.initApp();
-                break;
-        }
-    },
+	receivedEvent: function (event) {
+		switch (event) {
+			case 'deviceready':
+				app.initApp();
+				break;
+		}
+	},
 
-	initApp: function() {
+	initApp: function () {
+		Template7.registerHelper('if_compare', function (a, operator, b, options) {
+			var match = false;
+			if ((operator === '==' && a == b) || (operator === '===' && a === b) || (operator === '!=' && a != b)
+				|| (operator === '>' && a > b) || (operator === '<' && a < b) || (operator === '>=' && a >= b)
+				|| (operator === '<=' && a <= b)) {
+				match = true;
+			}
+
+			if (match) {
+				return options.fn(this);
+			} else {
+				return options.inverse(this);
+			}
+		});
+
 		window.$$ = Dom7;
 		window.gConfig = config;
 		window.gUser = storage.getUser();
 		window.gRepository = repository.getRepository();
 
-        window.nrApp = new Framework7({
-            pushState: false,
+		window.nrApp = new Framework7({
+			pushState: false,
 			smartSelectInPopup: true,
 			smartSelectPopupCloseText: '',
 			smartSelectPopupCloseTemplate: '<div class="left"></div>',
-            animateNavBackIcon: true,
-            modalTitle: config.modalTitle,
-            modalButtonOk: config.modalButtonOk,
-            modalButtonCancel: config.modalButtonCancel,
-            template7Pages: true
-        });
+			animateNavBackIcon: true,
+			modalTitle: config.modalTitle,
+			modalButtonOk: config.modalButtonOk,
+			modalButtonCancel: config.modalButtonCancel,
+			template7Pages: true
+		});
 
 		window.log = function () {
 			if (!config.debug) {
@@ -58,23 +75,23 @@ var app = {
 			console.log.apply(console, arguments);
 		};
 
-        nrApp.addView('#homeView', {
-            dynamicNavbar: true
-        });
+		nrApp.addView('#homeView', {
+			dynamicNavbar: true
+		});
 
-        nrApp.addView('#settingView', {
-            dynamicNavbar: true
-        });
+		nrApp.addView('#settingView', {
+			dynamicNavbar: true
+		});
 
-        router.init();
+		router.init();
 
 		home.init();
 		setting.init();
 
-        if (utils.isEmpty(gUser)) {
+		if (utils.isEmpty(gUser)) {
 			login.show();
 		}
-    }
+	}
 };
 
 app.initialize();
